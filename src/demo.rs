5801e6f9bf94c2223b5853e1d737c30951a93d6e
Richard Cook <rcook@rcook.org>
2025-05-27 00:00:20 -0700

    Whitespace

--- src/demo.rs
+++ src/demo.rs
@@ -6,18 +6,13 @@ use std::thread::spawn;
 
 pub(crate) fn demo() -> Result<()> {
     let args = Args::parse();
-
     let program_info = Some(ProgramInfo::new(&args.path, args.start));
-
     let debug_channel = channel();
     let status_channel = channel();
     let mut ui = UI::new(status_channel.1, debug_channel.0)?;
-
     spawn(move || {
         run_vm(debug_channel.1, status_channel.0, program_info).expect("Must succeed");
     });
-
     ui.run();
-
     Ok(())
 }
