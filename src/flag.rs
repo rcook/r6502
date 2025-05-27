e1349d259883bd3eff19e9454ef899f5a4cd93aa
Richard Cook <rcook@rcook.org>
2025-05-26 23:51:09 -0700

    Add more members to Flag

--- src/flag.rs
+++ src/flag.rs
@@ -2,7 +2,13 @@
 #[repr(u8)]
 pub(crate) enum Flag {
     N = 0b1000_0000u8,
+    #[allow(unused)]
+    V = 0b0100_0000u8,
     B = 0b0001_0000u8,
+    #[allow(unused)]
+    D = 0b0000_1000u8,
+    #[allow(unused)]
+    I = 0b0000_0100u8,
     Z = 0b0000_0010u8,
     Carry = 0b0000_0001u8,
 }
