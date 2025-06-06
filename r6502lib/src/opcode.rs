056c1d4156d8312932375d223a03e1dcb7e594d7
Richard Cook <rcook@rcook.org>
2025-06-05 21:31:11 -0700

    Fix mnemonic

--- r6502lib/src/opcode.rs
+++ r6502lib/src/opcode.rs
@@ -73,7 +73,7 @@ pub enum Opcode {
     Bvs = 0x70,
     #[strum(props(mnemonic = "CLC"))]
     Clc = 0x18,
-    #[strum(props(mnemonic = "ClD"))]
+    #[strum(props(mnemonic = "CLD"))]
     Cld = 0xd8,
     #[strum(props(mnemonic = "CLI"))]
     Cli = 0x58,
