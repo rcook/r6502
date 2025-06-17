3b969216595fb224ca4b8ef7c5b9d17c3b4d329e
Richard Cook <rcook@rcook.org>
2025-06-16 20:32:12 -0700

    Add other symbol types

--- r6502lib/src/symbols/export_kind.rs
+++ r6502lib/src/symbols/export_kind.rs
@@ -2,6 +2,15 @@ use strum_macros::EnumString;
 
 #[derive(Debug, EnumString, PartialEq)]
 pub enum ExportKind {
+    #[strum(serialize = "EA")]
+    Ea,
+
+    #[strum(serialize = "LA")]
+    La,
+
+    #[strum(serialize = "LZ")]
+    Lz,
+
     #[strum(serialize = "REA")]
     Rea,
 
