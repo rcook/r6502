f6c1fa58fa0398f7df5a68ec2291a994569443b2
Richard Cook <rcook@rcook.org>
2025-06-17 10:46:52 -0700

    Move r6502 image handling code into separate module

--- r6502lib/src/emulator/image.rs
+++ r6502lib/src/emulator/image.rs
@@ -1,10 +1,9 @@
 use crate::emulator::util::make_word;
 use crate::emulator::{
-    AddressRange, ImageHeader, ImageSlice, MachineTag, R6502ImageType, R6502_MAGIC_NUMBER,
+    read_r6502_image_header, AddressRange, ImageHeader, ImageSlice, MachineTag,
     SIM6502_MAGIC_NUMBER,
 };
 use anyhow::{bail, Error, Result};
-use num_traits::FromPrimitive;
 use std::fs::File;
 use std::io::{Cursor, ErrorKind, Read, Seek};
 use std::path::Path;
@@ -132,7 +131,7 @@ impl Image {
     }
 
     fn read_header<R: Read + Seek>(reader: &mut R) -> Result<ImageHeader> {
-        let header = Self::read_r6502_header(reader)?;
+        let header = read_r6502_image_header(reader)?;
         if let Some(header) = header {
             return Ok(header);
         }
@@ -143,65 +142,6 @@ impl Image {
         }
 
         Ok(ImageHeader::None)
-    }
-
-    fn read_r6502_header<R: Read + Seek>(reader: &mut R) -> Result<Option<ImageHeader>> {
-        macro_rules! fill_buffer {
-            ($reader: expr, $buffer: expr) => {
-                match $reader.read_exact($buffer) {
-                    Ok(()) => {}
-                    Err(e) if e.kind() == ErrorKind::UnexpectedEof => {
-                        $reader.rewind()?;
-                        return Ok(None);
-                    }
-                    Err(e) => anyhow::bail!(e),
-                }
-            };
-        }
-
-        macro_rules! read_le_word {
-            ($reader: expr) => {{
-                let mut bytes = [0x00; 2];
-                fill_buffer!($reader, &mut bytes);
-                u16::from_le_bytes(bytes)
-            }};
-        }
-
-        macro_rules! read_byte {
-            ($reader: expr) => {{
-                let mut bytes = [0x00; 1];
-                fill_buffer!($reader, &mut bytes);
-                bytes[0]
-            }};
-        }
-
-        fn read_type0_header<R: Read + Seek>(reader: &mut R) -> Result<Option<ImageHeader>> {
-            let mut machine_tag = MachineTag::default();
-            fill_buffer!(reader, &mut machine_tag);
-            let load = read_le_word!(reader);
-            let start = read_le_word!(reader);
-            Ok(Some(ImageHeader::R6502Type0 {
-                machine_tag,
-                load,
-                start,
-            }))
-        }
-
-        let magic_number = read_le_word!(reader);
-        if magic_number != R6502_MAGIC_NUMBER {
-            reader.rewind()?;
-            return Ok(None);
-        }
-
-        let Some(image_type) = R6502ImageType::from_u8(read_byte!(reader)) else {
-            reader.rewind()?;
-            return Ok(None);
-        };
-
-        Ok(match image_type {
-            R6502ImageType::Type0 => read_type0_header(reader)?,
-            _ => todo!(),
-        })
     }
 
     fn read_sim6502_header<R: Read + Seek>(reader: &mut R) -> Result<Option<ImageHeader>> {
---
+++ r6502lib/src/emulator/r6502_image.rs
@@ -0,0 +1,63 @@
+use crate::emulator::{ImageHeader, MachineTag, R6502ImageType, R6502_MAGIC_NUMBER};
+use anyhow::Result;
+use num_traits::FromPrimitive;
+use std::io::{ErrorKind, Read, Seek};
+
+pub fn read_r6502_image_header<R: Read + Seek>(reader: &mut R) -> Result<Option<ImageHeader>> {
+    macro_rules! fill_buffer {
+        ($reader: expr, $buffer: expr) => {
+            match $reader.read_exact($buffer) {
+                Ok(()) => {}
+                Err(e) if e.kind() == ErrorKind::UnexpectedEof => {
+                    $reader.rewind()?;
+                    return Ok(None);
+                }
+                Err(e) => anyhow::bail!(e),
+            }
+        };
+    }
+
+    macro_rules! read_le_word {
+        ($reader: expr) => {{
+            let mut bytes = [0x00; 2];
+            fill_buffer!($reader, &mut bytes);
+            u16::from_le_bytes(bytes)
+        }};
+    }
+
+    macro_rules! read_byte {
+        ($reader: expr) => {{
+            let mut bytes = [0x00; 1];
+            fill_buffer!($reader, &mut bytes);
+            bytes[0]
+        }};
+    }
+
+    fn read_type0_header<R: Read + Seek>(reader: &mut R) -> Result<Option<ImageHeader>> {
+        let mut machine_tag = MachineTag::default();
+        fill_buffer!(reader, &mut machine_tag);
+        let load = read_le_word!(reader);
+        let start = read_le_word!(reader);
+        Ok(Some(ImageHeader::R6502Type0 {
+            machine_tag,
+            load,
+            start,
+        }))
+    }
+
+    let magic_number = read_le_word!(reader);
+    if magic_number != R6502_MAGIC_NUMBER {
+        reader.rewind()?;
+        return Ok(None);
+    }
+
+    let Some(image_type) = R6502ImageType::from_u8(read_byte!(reader)) else {
+        reader.rewind()?;
+        return Ok(None);
+    };
+
+    Ok(match image_type {
+        R6502ImageType::Type0 => read_type0_header(reader)?,
+        _ => todo!(),
+    })
+}
