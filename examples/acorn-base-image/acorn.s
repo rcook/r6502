b284d7662f505546662b61571eae005fac5b671a
Richard Cook <rcook@rcook.org>
2025-06-18 09:02:12 -0700

    Comments

--- examples/acorn-base-image/acorn.s
+++ examples/acorn-base-image/acorn.s
@@ -1,3 +1,10 @@
+; Eventually this will implement a tiny subset of Acorn MOS 1.00/1.20 functionality
+; to support character I/O: so, most likely, just OSRDCH and OSWRCH
+; Sources:
+;   https://github.com/raybellis/mos120
+;   https://mdfs.net/Docs/Comp/BBC/OS1-20/
+;   https://tobylobster.github.io/mos/mos/index.html
+
 .export STARTUP
 
 ; r6502 executable header
