a708121f6e37038d5a896864c7d50f75dce0e9f4
Richard Cook <rcook@rcook.org>
2025-06-18 09:06:32 -0700

    Another link

--- examples/acorn-base-image/acorn.s
+++ examples/acorn-base-image/acorn.s
@@ -4,6 +4,7 @@
 ;   https://github.com/raybellis/mos120
 ;   https://mdfs.net/Docs/Comp/BBC/OS1-20/
 ;   https://tobylobster.github.io/mos/mos/index.html
+;   https://www.pa3byb.nl/wp-content/uploads/2017/12/Advance_user_guide_Acorn_electron.pdf
 
 .export STARTUP
 
