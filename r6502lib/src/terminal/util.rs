aa6618d2a1364d4658aa739e840ff5b1b5c4f130
Richard Cook <rcook@rcook.org>
2025-06-18 11:39:33 -0700

    Write to log instead of stdout

--- r6502lib/src/terminal/util.rs
+++ r6502lib/src/terminal/util.rs
@@ -1,11 +1,12 @@
 use crate::emulator::Image;
 use crate::run_options::RunOptions;
+use log::info;
 use std::str::from_utf8;
 
 pub fn show_image_info(opts: &RunOptions, image: &Image, start: u16) {
-    println!("Image: {}", opts.path.display());
+    info!("Image: {}", opts.path.display());
 
-    println!(
+    info!(
         "  {label:<25}: {s} (${s:04X}) bytes",
         label = "Image size",
         s = image.bytes().len()
@@ -13,32 +14,32 @@ pub fn show_image_info(opts: &RunOptions, image: &Image, start: u16) {
 
     match image.machine_tag() {
         Some(tag) => {
-            println!(
+            info!(
                 "  {label:<25}: {tag}",
                 label = "Format",
                 tag = from_utf8(&tag).expect("Must be valid UTF-8")
             );
         }
         None => {
-            println!("  {label:<25}: (unspecified)", label = "Machine tag",);
+            info!("  {label:<25}: (unspecified)", label = "Machine tag",);
         }
     }
 
-    println!(
+    info!(
         "  {label:<25}: ${load:04X}",
         label = "Load address",
         load = image.load().or(opts.load).unwrap_or_default()
     );
 
     if opts.reset {
-        println!(
+        info!(
             "  {label:<25}: ${start:04X} (RESET, overriding ${original_start:04X})",
             label = "Start address",
             start = start,
             original_start = image.start().or(opts.start).unwrap_or_default()
         );
     } else {
-        println!(
+        info!(
             "  {label:<25}: ${start:04X}",
             label = "Start address",
             start = image.start().or(opts.start).unwrap_or_default()
@@ -47,14 +48,14 @@ pub fn show_image_info(opts: &RunOptions, image: &Image, start: u16) {
 
     match image.sp() {
         Some(sp) => {
-            println!(
+            info!(
                 "  {label:<25}: ${sp:02X}",
                 label = "Initial stack pointer",
                 sp = sp
             );
         }
         None => {
-            println!(
+            info!(
                 "  {label:<25}: (unspecified)",
                 label = "Initial stack pointer",
             );
@@ -62,6 +63,6 @@ pub fn show_image_info(opts: &RunOptions, image: &Image, start: u16) {
     }
 
     if let Some(stop_after) = opts.stop_after {
-        println!("  {label:<25}: {stop_after} cycles", label = "Stop after");
+        info!("  {label:<25}: {stop_after} cycles", label = "Stop after");
     }
 }
