8b6c3726522f01b48ccdc8a93f042785117a24dc
Richard Cook <rcook@rcook.org>
2025-06-23 17:17:10 -0700

    Working on Windows

---
+++ examples/shared.mk
@@ -0,0 +1,9 @@
+CC65DIR := $(shell dirname $$(dirname $$(readlink -f $$(which ld65))))
+ifeq ($(OS),Windows_NT)
+CC65DIR := $(shell echo $(CC65DIR) | sed -e 's/\/\(.\)\//\\1:\//g')
+endif
+ifeq ($(OS),Windows_NT)
+CC65LIBDIR := $(CC65DIR)/lib
+else
+CC65LIBDIR := $(CC65DIR)/share/cc65/lib
+endif
