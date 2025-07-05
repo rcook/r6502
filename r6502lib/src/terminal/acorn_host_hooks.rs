5b596bd7792c66b23b8801d710916556861f34aa
Richard Cook <rcook@rcook.org>
2025-07-05 00:14:06 -0700

    Implement OSFILE $FF

--- r6502lib/src/terminal/acorn_host_hooks.rs
+++ r6502lib/src/terminal/acorn_host_hooks.rs
@@ -5,7 +5,7 @@ use anyhow::{anyhow, bail, Result};
 use log::info;
 use std::env::current_dir;
 use std::fs::{read_dir, File};
-use std::io::{BufWriter, ErrorKind, Write};
+use std::io::{BufReader, BufWriter, ErrorKind, Read, Write};
 
 const OSAREG: u16 = 0x00ef; // BASIC zero-page workspace value
 const HOOK_OK: u8 = 0;
@@ -37,11 +37,13 @@ fn handle_cliv(cpu: &mut Cpu) -> Result<()> {
 }
 
 fn handle_filev(cpu: &mut Cpu) -> Result<()> {
-    let result = if cpu.reg.a == 0 {
-        save_memory(cpu)?
-    } else {
-        info!("OSFILE operation ${a:02X} not implemented", a = cpu.reg.a);
-        false
+    let result = match cpu.reg.a {
+        0 => save_memory(cpu)?,
+        0xff => load_memory(cpu)?,
+        _ => {
+            info!("OSFILE operation ${a:02X} not implemented", a = cpu.reg.a);
+            false
+        }
     };
     cpu.reg.a = if result { HOOK_OK } else { HOOK_BRK };
     Ok(())
@@ -62,24 +64,6 @@ fn show_catalogue() -> Result<bool> {
 }
 
 fn save_memory(cpu: &mut Cpu) -> Result<bool> {
-    fn read_word(cpu: &mut Cpu, base_addr: u16, offset: u16) -> u16 {
-        let lo_addr = base_addr.wrapping_add(offset);
-        let hi_addr = lo_addr.wrapping_add(1);
-        make_word(cpu.bus.load(hi_addr), cpu.bus.load(lo_addr))
-    }
-
-    fn read_dword(cpu: &mut Cpu, base_addr: u16, offset: u16) -> u32 {
-        let b0_addr = base_addr.wrapping_add(offset);
-        let b1_addr = b0_addr.wrapping_add(1);
-        let b2_addr = b1_addr.wrapping_add(1);
-        let b3_addr = b2_addr.wrapping_add(1);
-        let b0 = cpu.bus.load(b0_addr) as u32;
-        let b1 = cpu.bus.load(b1_addr) as u32;
-        let b2 = cpu.bus.load(b2_addr) as u32;
-        let b3 = cpu.bus.load(b3_addr) as u32;
-        (b3 << 24) + (b2 << 16) + (b1 << 8) + b0
-    }
-
     let xy_addr = make_word(cpu.reg.y, cpu.reg.x);
     let file_name_addr = read_word(cpu, xy_addr, 0);
     let load_addr = read_dword(cpu, xy_addr, 2);
@@ -150,6 +134,68 @@ fn save_memory(cpu: &mut Cpu) -> Result<bool> {
     Ok(true)
 }
 
+fn load_memory(cpu: &mut Cpu) -> Result<bool> {
+    let xy_addr = make_word(cpu.reg.y, cpu.reg.x);
+    let file_name_addr = read_word(cpu, xy_addr, 0);
+    let load_addr = read_dword(cpu, xy_addr, 2);
+    let execution_addr = read_dword(cpu, xy_addr, 6);
+    let start_addr = read_dword(cpu, xy_addr, 10);
+    let end_addr = read_dword(cpu, xy_addr, 14);
+    let file_name = read_cr_terminated_string(cpu, file_name_addr)?;
+
+    info!(
+        "Loading {file_name} {load_addr:08X} {execution_addr:08X} {start_addr:08X} {end_addr:08X}",
+    );
+
+    let load_addr = u16::try_from(load_addr & 0xffff).unwrap();
+
+    let d = current_dir()?;
+    let p = d.join(&file_name);
+
+    let f = match File::open(&p) {
+        Ok(f) => f,
+        Err(e) => {
+            if e.kind() == ErrorKind::NotFound {
+                info!("File {path} not found", path = p.display());
+            } else {
+                info!(
+                    "Error occurred while attempting to save to {path}: {e}",
+                    path = p.display()
+                );
+            }
+            return Ok(false);
+        }
+    };
+    let mut reader = BufReader::new(f);
+    let mut bytes = [0; 1];
+    let mut count = 0;
+    for addr in load_addr.. {
+        match reader.read_exact(&mut bytes) {
+            Ok(()) => {}
+            Err(e) => {
+                if e.kind() == ErrorKind::UnexpectedEof {
+                    break;
+                }
+                info!(
+                    "Error occurred while attempting to load {path}: {e}",
+                    path = p.display()
+                );
+                return Ok(false);
+            }
+        }
+        cpu.bus.store(addr, bytes[0]);
+        count += 1;
+    }
+
+    write_dword(cpu, xy_addr, 10, count);
+
+    info!(
+        "Successfully loaded {file_name} {load_addr:08X} {execution_addr:08X} {start_addr:08X} {end_addr:08X}",
+    );
+
+    Ok(true)
+}
+
 fn read_cr_terminated_string(cpu: &Cpu, start_addr: u16) -> Result<String> {
     let mut s = String::with_capacity(200);
     for addr in start_addr.. {
@@ -160,4 +206,36 @@ fn read_cr_terminated_string(cpu: &Cpu, start_addr: u16) -> Result<String> {
         s.push(byte as char);
     }
     bail!("string at ${:04X} is too long", start_addr)
+}
+
+fn read_word(cpu: &mut Cpu, base_addr: u16, offset: u16) -> u16 {
+    let lo_addr = base_addr.wrapping_add(offset);
+    let hi_addr = lo_addr.wrapping_add(1);
+    make_word(cpu.bus.load(hi_addr), cpu.bus.load(lo_addr))
+}
+
+fn read_dword(cpu: &mut Cpu, base_addr: u16, offset: u16) -> u32 {
+    let b0_addr = base_addr.wrapping_add(offset);
+    let b1_addr = b0_addr.wrapping_add(1);
+    let b2_addr = b1_addr.wrapping_add(1);
+    let b3_addr = b2_addr.wrapping_add(1);
+    let b0 = cpu.bus.load(b0_addr) as u32;
+    let b1 = cpu.bus.load(b1_addr) as u32;
+    let b2 = cpu.bus.load(b2_addr) as u32;
+    let b3 = cpu.bus.load(b3_addr) as u32;
+    (b3 << 24) + (b2 << 16) + (b1 << 8) + b0
+}
+
+fn write_dword(cpu: &mut Cpu, base_addr: u16, offset: u16, value: u32) {
+    let b0_addr = base_addr.wrapping_add(offset);
+    let b1_addr = b0_addr.wrapping_add(1);
+    let b2_addr = b1_addr.wrapping_add(1);
+    let b3_addr = b2_addr.wrapping_add(1);
+    cpu.bus.store(b0_addr, u8::try_from(value & 0xff).unwrap());
+    cpu.bus
+        .store(b1_addr, u8::try_from((value >> 8) & 0xff).unwrap());
+    cpu.bus
+        .store(b2_addr, u8::try_from((value >> 16) & 0xff).unwrap());
+    cpu.bus
+        .store(b3_addr, u8::try_from((value >> 24) & 0xff).unwrap());
 }
