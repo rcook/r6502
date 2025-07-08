d49d7416a9cb8f4c1816473cccf8e95015d1d9fd
Richard Cook <rcook@rcook.org>
2025-07-08 13:04:06 -0700

    Add more ZP definitions

--- examples/acorn/mos_zp.s
+++ examples/acorn/mos_zp.s
@@ -1,8 +1,13 @@
 ; Acorn MOS zero-page workspace
 ; https://mdfs.net/Docs/Comp/BBC/AllMem
-.exportzp OSAREG = $EF
-.exportzp OSXREG = $F0
-.exportzp OSYREG = $F1
-.exportzp OSINTA = $FC
-.exportzp OSFAULT = $FD
-.exportzp OSESC = $FF
+; https://tobylobster.github.io/mos/os120_acme.a
+.exportzp OSVDU  = $D0      ; a.k.a. .vduStatusByte
+.exportzp OSKBD1 = $EC      ; a.k.a. .keyPressedInternalTable/.lastKeyPressedInternal
+.exportzp OSKBD2 = $ED      ; a.k.a. .firstKeyPressedInternal
+.exportzp OSKBD3 = $EE      ; a.k.a. .keyToIgnoreWhenScanningWithOSBYTE121or122
+.exportzp OSAREG = $EF      ; a.k.a. .osbyteA/.oswordA
+.exportzp OSXREG = $F0      ; a.k.a. .osbyteX/.oswordX/.stackPointerLastBRK
+.exportzp OSYREG = $F1      ; a.k.a. .osbyteY/.oswordY
+.exportzp OSINTA = $FC      ; a.k.a. .interruptAccumulator
+.exportzp OSFAULT = $FD     ; a.k.a. .brkAddressLow
+.exportzp OSESC = $FF       ; a.k.a. .escapeFlag
